36da15565125cd9947cd78a16da0354b1930cda5
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-20 21:11:16 -0800

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -4,3 +4,9 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+
+// API URL > https://www.googleapis.com/books/v1/volumes?q=peterhopkirk&maxResults=40&key=yourAPIKey 
+// q={search results} 
+// maxResults={total # of results, 40 is most provided by Google Books API} 
+// key={my API key, it is encoded and no need to hide. Simply store in env.local and check URL to confrim security}
