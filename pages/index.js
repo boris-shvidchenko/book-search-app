81a8d3f671388b0a39c24e550ecb7f17c15e7657
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-21 09:48:55 -0800

    Added API key to local env file, make initial call to API to test out fetch

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 // Components
 import Head from 'next/head';
 
+// Hooks
+import { useEffect } from 'react';
+
 export default function Home() {
+
+  useEffect(() => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=peterhopkirk&maxResults=40&key=${process.env.NEXT_PUBLIC_API_KEY}`)
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <>
       <Head>
@@ -12,7 +22,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Book Search</title>
       </Head>
-      <main></main>
+      <main>
+        
+      </main>
     </>
   )
 }
+
+// API URL > https://www.googleapis.com/books/v1/volumes?q=peterhopkirk&maxResults=40&key=yourAPIKey 
+// q={search results} 
+// maxResults={total # of results, 40 is most provided by Google Books API} 
+// key={my API key, it is encoded and no need to hide. Simply store in env.local and check URL to confrim security}
