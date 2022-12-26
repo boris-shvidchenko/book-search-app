fd9ec010c5df01845b2c015e379558987bfb1f0b
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-26 12:16:22 -0800

    Included spinner loader from react loader spinner module

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,14 +1,27 @@
 // Hooks
 import { usePromiseTracker } from 'react-promise-tracker';
 
+// Spinner
+import { TailSpin } from 'react-loader-spinner';
+
 export default function LoadingScreen() {
 
     const { promiseInProgress } = usePromiseTracker(); 
     
     return (
         promiseInProgress &&
-        <main className='absolute top-0 text-white bg-black/80 w-full h-full overflow-hidden z-50'>
-            <div className='flex flex-col w-80 mx-auto mt-56 text-center space-y-6'>Loading</div>
+        <main className='loading-scrn-main'>
+            <div className='loading-scrn-spinner'>
+                <TailSpin
+                    height="80"
+                    width="80"
+                    color="#f55b14"
+                    ariaLabel="tail-spin-loading"
+                    radius="1"
+                    wrapperStyle={{}}
+                    visible={true}
+                />
+            </div>
         </main>
     )
 }
